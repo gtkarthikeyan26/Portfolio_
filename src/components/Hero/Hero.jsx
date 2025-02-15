e0547cd6e3d49d4664efad4c5460a0fd7de83db8
gtkarthikeyan26 <gtkarthikeyan@gmail.com>
2025-02-15 21:03:45 +0530

    changes1

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
             <h1>Hi, I'm Karthikeyan</h1>
             <p className={styles.description}>
                     I'm a Software Developer with experience in Html, Css, JavaScript, React, MySQL. 
-                    Reach out to me if you'd like to connect!
+                    Reach out to me if you'd like to connect!.
                 </p>
                 <a href="https://www.linkedin.com/in//" className={styles.contactBtn}>Contact me</a>
             </div>
